63bb18d305a670d721ac31812f21bd5fac6574ab
Elijah Odjegba <elijahodjegba@gmail.com>
2023-03-14 20:14:40 +0100

    Add items controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,22 @@
+class ItemsController < ApplicationController
+    def new
+      @item = Item.new
+    end
+  
+    def create
+      @item = Item.new(item_params)
+  
+      if @item.save
+        redirect_to root_path, notice: "Item created successfully!"
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def item_params
+      params.require(:item).permit(:name, :description, :price)
+    end
+  end
+  
